b5f6f89b33cb9f533aaee9e61be75df0331169c2
kikidy12 <30651543+kikidy12@users.noreply.github.com>
2019-09-30 10:17:44 +0900

    Initial Commit

---
+++ woorischool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  woorischool
+//
+//  Created by 권성민 on 30/09/2019.
+//  Copyright © 2019 beanfactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ woorischoolTests/woorischoolTests.swift
@@ -0,0 +1,34 @@
+//
+//  woorischoolTests.swift
+//  woorischoolTests
+//
+//  Created by 권성민 on 30/09/2019.
+//  Copyright © 2019 beanfactory. All rights reserved.
+//
+
+import XCTest
+@testable import woorischool
+
+class woorischoolTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
